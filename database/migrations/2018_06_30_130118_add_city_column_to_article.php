e7f92971384e18617172038fcb82d9fa74458072
Zuip <zuiohi@gmail.com>
2018-07-01 15:32:17 +0300

    Added a new column in the article sql table for city

---
+++ database/migrations/2018_06_30_130118_add_city_column_to_article.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCityColumnToArticle extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('article', function($table) {
+            $table->integer('city_id')->nullable();
+            $table->foreign('city_id')->references('id')->on('city');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
