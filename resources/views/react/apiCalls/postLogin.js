4729b2a0e7536e43160bf809b5d526c0595c7ef6
Zuip <zuiohi@gmail.com>
2018-07-28 10:34:09 +0300

    Logging in to the system now also logs in to the admin page

--- resources/views/react/apiCalls/postLogin.js
+++ resources/views/react/apiCalls/postLogin.js
@@ -1,19 +1,35 @@
 export default function(username, password) {
-  return fetch(
-    '/api/login',
-    {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        'X-CSRF-TOKEN': csrf_token
-      },
-      body: JSON.stringify({
-        username,
-        password
-      }),
-      credentials: 'same-origin'
-    }
-  ).then(
-    response => response.json()
+  return Promise.all([
+    fetch(
+      '/api/login',
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          'X-CSRF-TOKEN': csrf_token
+        },
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        credentials: 'same-origin'
+      }
+    ),
+    fetch(
+      '/admin/api/login',
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        credentials: 'same-origin'
+      }
+    )
+  ]).then(
+    response => response[0].json()
   );
 };
--- resources/views/react/apiCalls/postLogout.js
+++ resources/views/react/apiCalls/postLogout.js
@@ -1,13 +1,27 @@
 export default function(username, password) {
-  return fetch(
-    '/api/logout',
-    {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json',
-        'X-CSRF-TOKEN': csrf_token
-      },
-      credentials: 'same-origin'
-    }
+  return Promise.all([
+    fetch(
+      '/api/logout',
+      {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          'X-CSRF-TOKEN': csrf_token
+        },
+        credentials: 'same-origin'
+      }
+    ),
+    fetch(
+      '/admin/api/logout',
+      {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json'
+        },
+        credentials: 'same-origin'
+      }
+    )
+  ]).then(
+    response => response[0]
   );
 };
