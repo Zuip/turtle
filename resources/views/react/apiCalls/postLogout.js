8375d125697fc29a4fba21013c632435c0307bf5
Zuip <zuiohi@gmail.com>
2018-08-26 11:45:07 +0300

    Changed session storage from file storage to redis storage

--- resources/views/react/apiCalls/postLogout.js
+++ resources/views/react/apiCalls/postLogout.js
@@ -1,27 +1,13 @@
-export default function(username, password) {
-  return Promise.all([
-    fetch(
-      '/api/logout',
-      {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-          'X-CSRF-TOKEN': csrf_token
-        },
-        credentials: 'same-origin'
-      }
-    ),
-    fetch(
-      '/admin/api/logout',
-      {
-        method: 'GET',
-        headers: {
-          'content-type': 'application/json'
-        },
-        credentials: 'same-origin'
-      }
-    )
-  ]).then(
-    response => response[0]
+export default function() {
+  return fetch(
+    '/api/logout',
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'X-CSRF-TOKEN': csrf_token
+      },
+      credentials: 'same-origin'
+    }
   );
 };
