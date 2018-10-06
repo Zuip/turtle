6f6cb1a2eefec6386e9e4c05e3b85ecc839ee753
Zuip <zuiohi@gmail.com>
2018-10-06 17:55:08 +0300

    Added two files that were forgotten from the previous commit

--- resources/views/react/components/Routes/wrappers/fi/Login.js
+++ resources/views/react/components/Routes/wrappers/fi/Login.js
@@ -6,6 +6,11 @@ import RouteWrapperParent from './RouteWrapperParent';
 
 class LoginRouteWrapper extends RouteWrapperParent {
   render() {
+
+    if(!this.state.loaded) {
+      return null;
+    }
+
     return (
       <Login match={this.props.match} history={this.props.history} />
     );
