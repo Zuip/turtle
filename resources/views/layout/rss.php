d5cbbff8415a411cda8d115000cfe8d739144fe1
Zuip <zuiohi@gmail.com>
2018-11-10 12:12:04 +0200

    Also added dates to the titles off rss feed articles

--- resources/views/layout/rss.php
+++ resources/views/layout/rss.php
@@ -7,8 +7,23 @@
     <description><?php echo $description; ?></description>
     <?php foreach($articles as $article) { ?>
     <item>
-      <title><?php echo $article["city"]["name"]; ?>, <?php echo $article["city"]["country"]["name"]; ?></title>
-      <link><?php echo env('APP_URL'); ?>/matkat/<?php echo $article["visit"]["trip"]["urlName"]; ?>/<?php echo $article["city"]["country"]["urlName"]; ?>/<?php echo $article["city"]["urlName"]; ?>/<?php echo $article["city"]["visit"]["index"] ?>/artikkeli</link>
+      <?php
+        $startDate = DateTime::createFromFormat("Y-m-d", $article["visit"]["start"]);
+        $endDate = DateTime::createFromFormat("Y-m-d", $article["visit"]["end"]);
+        $startFormat = "j.n.Y";
+        if($startDate->format("Y") === $endDate->format("Y")) {
+          $startFormat = "j.n.";
+          if($startDate->format("n") === $endDate->format("n")) {
+            $startFormat = "j.";
+          }
+        }
+        $visitDuration = $startDate->format($startFormat) . "-" . $endDate->format("j.n.Y");
+        if($startDate->getTimestamp() === $endDate->getTimestamp()) {
+          $visitDuration = $endDate->format("j.n.Y");
+        }
+      ?>
+      <title><?php echo $article["city"]["name"]; ?>, <?php echo $article["city"]["country"]["name"]; ?>, <?php echo $visitDuration; ?></title>
+      <link><?php echo env("APP_URL"); ?>/matkat/<?php echo $article["visit"]["trip"]["urlName"]; ?>/<?php echo $article["city"]["country"]["urlName"]; ?>/<?php echo $article["city"]["urlName"]; ?>/<?php echo $article["city"]["visit"]["index"] ?>/artikkeli</link>
       <description><?php echo trim(strip_tags($article["summary"])); ?></description>
     </item>
     <?php } ?>
